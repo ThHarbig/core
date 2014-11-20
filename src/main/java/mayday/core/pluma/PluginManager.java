78324610edd7c8ba4ac723b1652b3e7f2c070b52
guenterjaeger <guenter.jaeger@student.uni-tuebingen.de>
2014-11-20 16:34:58 +0100

    since java8 local plugins can no longer be loaded during start up if maday is started as a webstart application.

--- src/main/java/mayday/core/pluma/PluginManager.java
+++ src/main/java/mayday/core/pluma/PluginManager.java
@@ -140,7 +140,8 @@ public class PluginManager {
 			progress.setVisible(true); // core is loaded=10%
 		
 		// 1 - Scan local plugin dir for all present files, extend classpath, scan jars
-		if (MaydayDefaults.isWebstartApplication() ||
+		if (/*MaydayDefaults.isWebstartApplication() || */
+		//GJ (20.11.2014): due to the new security updates in Java 8 adding local source code during start up is no longer permitted!
 				!pluginRoot.startsWith(coreRoot))				
 			FilesystemScanner.getInstance().scan(this, new File(pluginRoot));
 
